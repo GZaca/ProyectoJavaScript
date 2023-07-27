76519678bc405d3d92c01c098009278cafccc4d1
GermanZ <german.zacarias.2792@gmail.com>
2023-07-27 16:49:01 -0300

    TercerPreEntrega

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -48,115 +48,146 @@ const stock = [
     new Prenda('Short', 7000, 'Baño', 'S/M'),
 ];
 
+function segundaEntrega() {
 // // Ciclo principal
-// let sumaPrecios = 0;
-// let ticket = []
-// while (true) {
-//     // Solicitamos al usuario ingresar una opción
-//     let opcion = prompt('Ingresa una opción:\n1. Ver información de una prenda\n2. Ver stock ordenado por precio de mayor a menor\n3. Salir e imprimir ticket');
+let sumaPrecios = 0;
+let ticket = []  
+while (true) {
+    // Solicitamos al usuario ingresar una opción
+    let opcion = prompt('Ingresa una opción:\n1. Ver información de una prenda\n2. Ver stock ordenado por precio de mayor a menor\n3. Salir e imprimir ticket');
 
-//     // Opción seleccionada
-//     if (opcion === '1') {
-//         // Generamos la lista de opciones de prendas para mostrar en el prompt
-//         let opciones = 'Opciones de prendas disponibles:\n\n';
-//         for (let i = 0; i < stock.length; i++) {
-//             opciones += `${i + 1}. ${stock[i].nombre}\n`;
-//         }
+    // Opción seleccionada
+    if (opcion === '1') {
+        // Generamos la lista de opciones de prendas para mostrar en el prompt
+        let opciones = 'Opciones de prendas disponibles:\n\n';
+        for (let i = 0; i < stock.length; i++) {
+            opciones += `${i + 1}. ${stock[i].nombre}\n`;
+        }
 
-//         // Solicitamos al usuario seleccionar una opción y obtener su información
-//         let opcionSeleccionada = prompt(`${opciones}\nIngresa el número de la prenda para ver su información:`);
+        // Solicitamos al usuario seleccionar una opción y obtener su información
+        let opcionSeleccionada = prompt(`${opciones}\nIngresa el número de la prenda para ver su información:`);
 
-//         // Verificamos si la opción seleccionada es válida
-//         if (opcionSeleccionada >= 1 && opcionSeleccionada <= stock.length) {
-//             let prendaSeleccionada = stock[opcionSeleccionada - 1];
+        // Verificamos si la opción seleccionada es válida
+        if (opcionSeleccionada >= 1 && opcionSeleccionada <= stock.length) {
+            let prendaSeleccionada = stock[opcionSeleccionada - 1];
 
-//             let informacionPrenda = prendaSeleccionada.obtenerInformacion();
-//             let precioConIVA = prendaSeleccionada.obtenerPrecioConIVA();
+            let informacionPrenda = prendaSeleccionada.obtenerInformacion();
+            let precioConIVA = prendaSeleccionada.obtenerPrecioConIVA();
 
-//             informacionPrenda += `\nPrecio con IVA: $${precioConIVA.toFixed(2)}`;
-//             alert(informacionPrenda);
+            informacionPrenda += `\nPrecio con IVA: $${precioConIVA.toFixed(2)}`;
+            alert(informacionPrenda);
 
-//             sumaPrecios += prendaSeleccionada.precio;
-//             ticket.push (prendaSeleccionada.nombre);
-//         } else {
-//             alert('La opción seleccionada no es válida, volvemos al home.');
-//         }
-//     } else if (opcion === '2') {
-//         // Ordenamos el stock por precio de mayor a menor utilizando el método sort
-//         stock.sort(function (a, b) {
-//             return b.precio - a.precio;
-//         });
-
-//         // Mostramos el stock ordenado 
-//         let listaStock = 'Stock ordenado por precio de mayor a menor (sin IVA):\n\n';
-//         for (let i = 0; i < stock.length; i++) {
-//             listaStock += `${stock[i].nombre} - Precio: $${stock[i].precio}\n`;
-//         }
-//         alert(listaStock);
-//     } else if (opcion === '3') {
-//         alert("Gracias por navegar, su ticket ha sido impreso en consola")
-
-//         console.log(ticket.join('\n '));
-//         console.log('La suma de los precios consultados (sin IVA) es: $' + sumaPrecios.toFixed(2));
-
-//         break; 
-//     } else {
-//         alert('Opción no válida');
-//     }
-// }
-// Salimos del ciclo e imrpimimos en consola si el usuario elige la opción "Salir"
-
-
-function crearCarritoDinamico() {
-    
-    const botonesAgregar = document.querySelectorAll('.agregar');
-    botonesAgregar.forEach(boton => {
-        boton.addEventListener('click', () => {
-            const nombre = boton.dataset.nombre;
-            const precio = parseFloat(boton.dataset.precio);
-
-            // Datos en el localStorage
-            const producto = { nombre, precio };
-            let productosEnCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
-            productosEnCarrito.push(producto);
-            localStorage.setItem('carrito', JSON.stringify(productosEnCarrito));
-
-            // Mostrar el carrito dinámicamente
-            const carritoDiv = document.createElement('div');
-            carritoDiv.classList.add('carrito');
-
-            const tituloCarrito = document.createElement('h2');
-            tituloCarrito.innerText = 'Carrito de Compras';
-            carritoDiv.appendChild(tituloCarrito);
-
-            const listaCarrito = document.createElement('ul');
-            listaCarrito.classList.add('lista-carrito');
-            carritoDiv.appendChild(listaCarrito);
-
-            const totalCarrito = document.createElement('p');
-            totalCarrito.classList.add('total');
-            let total = 0;
-            productosEnCarrito.forEach(producto => {
-                total += producto.precio;
-                const itemCarrito = document.createElement('li');
-                itemCarrito.innerText = `${producto.nombre} - $${producto.precio.toFixed(2)}`;
-                listaCarrito.appendChild(itemCarrito);
-            });
-            totalCarrito.innerHTML = `Total: $<span id="total">${total.toFixed(2)}</span>`;
-            carritoDiv.appendChild(totalCarrito);
-
-            // Si el carrito ya existe, eliminarlo para actualizarlo
-            const carritoExistente = document.querySelector('.carrito');
-            if (carritoExistente) {
-                carritoExistente.remove();
-            }
-
-            
-            document.body.appendChild(carritoDiv);
+            sumaPrecios += prendaSeleccionada.precio;
+            ticket.push (prendaSeleccionada.nombre);
+        } else {
+            alert('La opción seleccionada no es válida, volvemos al home.');
+        }
+    } else if (opcion === '2') {
+        // Ordenamos el stock por precio de mayor a menor utilizando el método sort
+        stock.sort(function (a, b) {
+            return b.precio - a.precio;
         });
-    });
+
+        // Mostramos el stock ordenado
+        let listaStock = 'Stock ordenado por precio de mayor a menor (sin IVA):\n\n';
+        for (let i = 0; i < stock.length; i++) {
+            listaStock += `${stock[i].nombre} - Precio: $${stock[i].precio}\n`;
+        }
+        alert(listaStock);
+    } else if (opcion === '3') {
+        alert("Gracias por navegar, su ticket ha sido impreso en consola")
+
+        console.log(ticket.join('\n '));
+        console.log('La suma de los precios consultados (sin IVA) es: $' + sumaPrecios.toFixed(2));
+
+        break;
+    } else {
+        alert('Opción no válida');
+    }
+    }
+}
+//Salimos del ciclo e imrpimimos en consola si el usuario elige la opción "Salir"
+
+
+//********************Pre-entrega 3 ***************** */
+
+
+// Guardar información en el localStorage
+
+function guardarEnLocalStorage(nombre, precio) {
+    const item = { nombre, precio };
+    let items = JSON.parse(localStorage.getItem('itemsSeleccionados')) || [];
+    items.push(item);
+    localStorage.setItem('itemsSeleccionados', JSON.stringify(items));
 }
 
-// Llamar a la función para crear el carrito dinámico cuando el DOM esté listo
-document.addEventListener('DOMContentLoaded', crearCarritoDinamico);
+// Función para mostrar el contenido del carrito
+
+function mostrarCarrito() {
+    const prendaSeleccionada = document.getElementById('itemsSeleccionados');
+    const totalCarrito = document.getElementById('total');
+
+    // Obtener desde el localStorage
+    const itemsSeleccionados = JSON.parse(localStorage.getItem('itemsSeleccionados'));
+
+    // Limpiar el contenido previo del carrito
+    prendaSeleccionada.innerHTML = '';
+    totalCarrito.textContent = '';
+
+    if (itemsSeleccionados && itemsSeleccionados.length > 0) {
+        const titulo = document.createElement('h3');
+        titulo.textContent = 'Prendas seleccionadas:';
+        prendaSeleccionada.appendChild(titulo);
+
+        // Crear una lista para mostrar los items seleccionados
+        const lista = document.createElement('ul');
+        let total = 0;
+        itemsSeleccionados.forEach(item => {
+            const listaItem = document.createElement('li');
+            listaItem.textContent = `${item.nombre} - $${item.precio}`;
+            lista.appendChild(listaItem);
+            total += item.precio;
+        });
+
+        prendaSeleccionada.appendChild(lista);
+        totalCarrito.textContent = total;
+    } else {
+        prendaSeleccionada.textContent = 'No hay items seleccionados';
+    }
+}
+
+
+// Función para borrar todo lo seleccionado
+function borrarSeleccion() {
+    localStorage.removeItem('itemsSeleccionados');
+    mostrarCarrito(); // Actualizar el carrito después de borrar
+}
+
+// Eventos para los botones
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addButtons = document.querySelectorAll('.add-btn');
+    const finalizarBtn = document.getElementById('finalizar-btn');
+    const arrepentimientoBtn = document.getElementById('arrepentimiento-btn');
+
+    addButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const nombre = button.dataset.nombre;
+            const precio = parseInt(button.dataset.precio);
+            guardarEnLocalStorage(nombre, precio);
+
+            // Mostrar el carrito actualizado después de agregar un producto
+            mostrarCarrito();
+        });
+    });
+
+    finalizarBtn.addEventListener('click', () => {
+        mostrarCarrito()
+        console.log("Muchas gracias por su compra ");
+    });
+
+    arrepentimientoBtn.addEventListener('click', () => {
+        borrarSeleccion();
+    });
+});
 
